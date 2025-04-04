c70c678bb4146e86d3ca1892118ad9618bd90658
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-04-04 22:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function updateTime() {
+            const now = new Date();
+            const formattedTime = now.toLocaleString();
+            document.getElementById('timer').textContent = formattedTime;
+        }
